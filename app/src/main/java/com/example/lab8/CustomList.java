1d0e441c3036a711db2c7c3483f51daf36372425
Yanmachine <hardingiant@gmail.com>
2023-03-09 18:25:31 -0700

    deleteCity Test. Test should succeed

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -61,7 +61,12 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void deleteCity(City city){
-
+        if (cities.contains(city)) {
+            cities.remove(city);
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
     }
 
 
