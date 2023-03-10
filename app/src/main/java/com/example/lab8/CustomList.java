cdba773e15e046bcd32be4c992bc951dfa6d1fbe
Yanmachine <hardingiant@gmail.com>
2023-03-09 18:08:16 -0700

    hasCity Test. Test should fail

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -53,4 +53,13 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
+    public boolean hasCity(City city){
+        return false;
+    }
+
+    public void deleteCity(){
+
+    }
+
+
 }
